fe64e65dcda79dcb85c749b34592ab6242da9ac1
ashuthe1 <ashutosh251848@gmail.com>
2024-10-30 14:17:13 +0530

    Better folder structure

--- src/solution.cpp
+++ src/solution.cpp
@@ -1,7 +1,17 @@
+// Ashutosh Gautam ツ
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+#ifdef AshutoshOS // It works on my machine.
+#include "../algo/debug.hpp" 
+#else
+#define deb(...)
+#endif
+
+#define int long long
+const int N = 1e6+10;
+
+void AshutoshGautam() {
     int n;
     cin >> n;
     int max_val = INT_MIN, min_val = INT_MAX;
@@ -9,9 +19,18 @@ int main() {
         int x;
         cin >> x;
         max_val = max(max_val, x);
-        min_val = min(min_val, x);
+        if(i < n-1) min_val = min(min_val, x);
     }
     cout << max_val << endl;
-    cout << min_val << endl;
+    cout << min_val<< endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int testCases = 1;
+    // cin >> testCases; 
+
+    for(int testCase = 1; testCase <= testCases ; testCase++)  
+        AshutoshGautam(); // Ping me for solving any issue ツ
     return 0;
 }
