e0ffc9fd260992d24e329d254ff7925c17acbf35
ashuthe1 <ashutosh251848@gmail.com>
2024-12-02 19:17:59 +0530

    Upsolve: after 11PM

--- code/solution.cpp
+++ code/solution.cpp
@@ -1,6 +1,4 @@
 // Ashutosh Gautam ツ
-// tag: special/segmentTree/addition_of_ap_in_range
-// Link: https://cses.fi/problemset/task/1736/
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -24,33 +22,19 @@ struct node {
     void merge(const node &leftChild, const node &rightChild) { 
         val = leftChild.val + rightChild.val;
     }
-    // Overloading << for printing
-    friend std::ostream& operator<<(std::ostream& os, const node& n) {
-        os << "{val: " << n.val << "}";
-        return os;
-    }
 };
 struct update {
-    int a = 0, d = 0;
+    int pending = 0; 
     update(){}
-    update(int a, int d) {
-        this->a = a; 
-        this->d = d;
+    update(int val) {
+        this->pending = val; 
     }
     void combine(update &newUpdate, const int &tl, const int &tr) {
-        a += newUpdate.a;
-        d += newUpdate.d;
+        pending += newUpdate.pending; 
     }
     // store the correct information in the node x
     void apply(node &cur, const int &tl, const int &tr){
-        int terms = (tr - tl + 1);
-        int extraSum = terms * a + (terms * (terms - 1) * d) / 2;
-        cur.val += extraSum;
-    }
-     // Overloading << for printing
-    friend std::ostream& operator<<(std::ostream& os, const update& u) {
-        os << "{a: " << u.a << ", d: " << u.d << "}";
-        return os;
+        cur.val += (tr - tl + 1) * pending;  // Adding value v in range from L to R
     }
 };
 
@@ -76,14 +60,8 @@ struct lazySegmentTree {
     void pushdown(const int &idx, const int &tl, const int &tr) {
         if(!lazy[idx]) return;
         int tm = (tl + tr) >> 1;
-
-        int terms = (tm - tl + 1);
-        update updRight = updates[idx];
-        updRight.a = updates[idx].a + terms * updates[idx].d;
-        updRight.d = updates[idx].d;
-
         apply(idx << 1, tl, tm, updates[idx]);
-        apply((idx << 1) | 1, tm + 1, tr, updRight);
+        apply((idx << 1) | 1, tm + 1, tr, updates[idx]);
         updates[idx] = identity_transformation;
         lazy[idx] = 0;
     }
@@ -130,16 +108,8 @@ struct lazySegmentTree {
         }
         pushdown(idx, tl, tr);
         int tm = (tl + tr) >> 1;
-
-        int terms = max(0LL, tm - max(l, tl) + 1); // did one type here and stuck for 2 hrs :(
-        update updRight = upd;
-        update updLeft = upd;
-
-        updRight.a = upd.a + terms * upd.d;
-        updRight.d = upd.d;
-
-        rangeUpdate(idx << 1, tl, tm, l, r, updLeft);
-        rangeUpdate((idx << 1) | 1, tm + 1, tr, l, r, updRight);
+        rangeUpdate(idx << 1, tl, tm, l, r, upd);
+        rangeUpdate((idx << 1) | 1, tm + 1, tr, l, r, upd);
         segTree[idx].merge(segTree[idx<<1], segTree[(idx << 1) | 1]);
     }
 
@@ -153,38 +123,48 @@ struct lazySegmentTree {
     void rangeUpdate(const int &l, const int &r, const update &upd) { // st.rangeUpdate(l, r, val)
         rangeUpdate(1, 0, len - 1, l, r, upd);
     }
-    void debug() {
-        for(int i = 1; i < 2*len; i++) {
-            deb(i, segTree[i]);
-            if(lazy[i]) {
-                deb(updates[i]);
-            }
-        }
-    }
 };
 
 void AshutoshGautam() {
     int n, q; cin >> n >> q;
-    vector<int> v(n+1, 0);
-    for(int i = 1; i <= n; i++) cin >> v[i];
+    vector<int> v(n+1, 0), ans(q+1), arr(n+1, 0);
+    vector<vector<int>> que[n+1];
 
-    lazySegmentTree st(n+1);
-    st.build(v);
-
-    while(q--) {
-        int t, l, r; cin >> t >> l >> r;
-        // deb(t, l, r);
-        // st.debug();
-        if(t == 1) {
-            st.rangeUpdate(l, r, update(1, 1));
-        } else {
-            int sum = st.query(l, r).val;
-            cout << sum << "\n";
-        }
-        deb(t, l, r);
-        st.debug();
-        deb("over")
+    for(int i = 1; i <= n; i++) {
+        cin >> v[i];
     }
+    for(int i = 1; i <= q; i++) {
+        int l, r; cin >> l >> r;
+        que[l].push_back({l, r, i});
+    }
+ 
+    lazySegmentTree st(n+1);
+    int prev = -1;
+    for(int i = 1; i <= n; i++) {
+        int cur = 0;
+        if(v[i] <= prev) {
+            cur = prev - v[i];
+        } 
+        arr[i] = cur;
+        prev = v[i] + cur;
+    }
+    st.build(arr);
+    deb(arr);
+
+    int prevExtra = 0;
+    for(int l = 1; l <= n; l++) {
+        deb(l, que[l]);
+        for(auto e: que[l]) {
+            int r = e[1], idx = e[2];
+            int sumTillR = st.query(l, r).val;
+            int operations = sumTillR + (r - l + 1) * prevExtra;
+            ans[idx] = operations;
+        }
+        if(v[l] >= v[l-1]) continue;
+        prevExtra += (v[l-1] - v[l]);
+    }
+ 
+    for(int i= 1; i <= q; i++) cout << ans[i] << "\n";
 }
 
 signed main() {
